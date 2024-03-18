ac490a625a440a35a4a3d72776f9abb1285d47f2
Jacob Broughton <jlbroughton88@gmail.com>
2024-03-17 20:55:00 -0400

    initialized with npm

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const express = require('express')
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port")
+})
