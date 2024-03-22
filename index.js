4defad86938b3c68a88341bc81ab4190ba729ad8
Jacob Broughton <jlbroughton88@gmail.com>
2024-03-21 22:06:57 -0400

    connected to postgres

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const express = require('express')
+const express = require("express")
+require('dotenv').config();
+const cors = require("cors");
+const { Client } = require("pg");
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.use(
+  cors({
+    origin: "chrome-extension://nbjkjkdhneemijlecobbdkodeecpfbjm",
+  })
+);
+
+const client = new Client({
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  ssl: true,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
+});
+
+app.get("/", async (req, res) => {
+  try {
+    await client.connect();
+    console.log("Connected to postgres database");
+  } catch (error) {
+    console.log("error connecting to postgressql database", error);
+  }
+
+  client.end()
+
+  res.send("Hello world");
+});
 
 app.listen(PORT, () => {
-  console.log("Server is listening on port")
-})
+  console.log(`Server is listening on port ${PORT}`);
+});
